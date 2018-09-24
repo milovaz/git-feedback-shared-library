222318c35403af191ecfaf318a1f42cd7ea07de4
Thiago Milo <milovaz@gmail.com>
2018-09-24 20:15:36 -0300

    changing paths and groovy file names

---
+++ vars/checkoutRepository.groovy
@@ -0,0 +1,34 @@
+#!/usr/bin/env groovy
+
+def call(String owner) {
+    def scmVars = checkout scm
+    def environment = scmVars.GIT_BRANCH == 'master' ? 'production' : 'development'
+    def description = "Deploying branch" 
+    def ref = scmVars.GIT_COMMIT
+    def splittedURL = scmVars.GIT_URL.split('/')
+    def repo = splittedURL.last().split('\\.').first()
+    owner = splittedURL[-2]
+    //def owner = "milovaz"
+    //def repo = "node-js-sample"
+    def deployURL = "https://api.github.com/repos/${owner}/${repo}/deployments"
+    def deployBody = '{"ref": "' + ref +'","environment": "' + environment  +'","description": "' + description + '", "auto_merge": false, "required_contexts": ["continuous-integration/jenkins/branch"]}'
+
+    // Create new Deployment using the GitHub Deployment API
+    def response = httpRequest authentication: 'github-user', httpMode: 'POST', requestBody: deployBody, responseHandle: 'STRING', url: deployURL
+    if(response.status != 201) {
+        error("Deployment API Create Failed: " + response.status)
+    }
+
+    // Get the ID of the GitHub Deployment just created
+    def responseJson = readJSON text: response.content
+    def id = responseJson.id
+    if(id == "") {
+        error("Could not extract id from Deployment response")
+    }
+
+    env.GIT_COMMIT = scmVars.GIT_COMMIT
+    env.GIT_BRANCH = scmVars.GIT_BRANCH
+    env.REPO_OWNER = owner
+    env.REPO_NAME = repo
+    env.DEPLOYMENT_ID = id
+}
--- vars/informGitDeployment.groovy
+++ vars/informGitDeployment.groovy
@@ -1,42 +1,12 @@
 #!/usr/bin/env groovy
 
-def call(String owner) {
-    def scmVars = checkout scm
-    def environment = "Prod"
-    def description = "Deploying my branch"
-    def ref = scmVars.GIT_COMMIT
-    def splittedURL = scmVars.GIT_URL.split('/')
-    def repo = splittedURL.last().split('\\.').first()
-    owner = splittedURL[-2]
-    //def owner = "milovaz"
-    //def repo = "node-js-sample"
-    def deployURL = "https://api.github.com/repos/${owner}/${repo}/deployments"
-    def deployBody = '{"ref": "' + ref +'","environment": "' + environment  +'","description": "' + description + '", "auto_merge": false, "required_contexts": ["continuous-integration/jenkins/branch"]}'
-
-    // Create new Deployment using the GitHub Deployment API
-    def response = httpRequest authentication: 'github-user', httpMode: 'POST', requestBody: deployBody, responseHandle: 'STRING', url: deployURL
-    if(response.status != 201) {
-        error("Deployment API Create Failed: " + response.status)
-    }
-
-    // Get the ID of the GitHub Deployment just created
-    def responseJson = readJSON text: response.content
-    def id = responseJson.id
-    if(id == "") {
-        error("Could not extract id from Deployment response")
-    }
-
-    // Execute Deployment
-    def deployStatus = sh returnStatus: true, script: 'echo deploy'
-
+def call(String owner, String repo, String id) {
     // Record new Deployment Status based on output
-    def result = (deployStatus) ? 'failure' : 'success'
+    def result = 'success'
     def deployStatusBody = '{"state": "' + result + '","target_url": "http://github.com/deploymentlogs"}'
     def deployStatusURL = "https://api.github.com/repos/${owner}/${repo}/deployments/${id}/statuses"
     def deployStatusResponse = httpRequest authentication: 'github-user', httpMode: 'POST', requestBody: deployStatusBody , responseHandle: 'STRING', url: deployStatusURL
     if(deployStatusResponse.status != 201) {
       error("Deployment Status API Update Failed: " + deployStatusResponse.status + " - " + deployStatusResponse.message)
     }
-
-    env.GIT_COMMIT = scmVars.GIT_COMMIT
 }
